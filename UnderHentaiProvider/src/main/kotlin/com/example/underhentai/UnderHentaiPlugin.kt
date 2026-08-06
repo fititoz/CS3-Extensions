cd58f46d7ba62e067185a46a69dbe82f5e7d6b41
fititoz <fititoz@users.noreply.github.com>
2026-08-05 20:23:09 -0400

    Register GUpload and LuluStream extractors in UnderHentaiPlugin

--- UnderHentaiProvider/src/main/kotlin/com/example/underhentai/UnderHentaiPlugin.kt
+++ UnderHentaiProvider/src/main/kotlin/com/example/underhentai/UnderHentaiPlugin.kt
@@ -2,10 +2,14 @@ package com.example.underhentai
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.extractors.GUpload
+import com.lagradost.cloudstream3.extractors.LuluStream
 
 @CloudstreamPlugin
 class UnderHentaiPlugin : BasePlugin() {
     override fun load() {
         registerMainAPI(UnderHentaiProvider())
+        registerExtractorAPI(GUpload())
+        registerExtractorAPI(LuluStream())
     }
 }
